3068b1ab984754cfe36e87165ebc9da0b55409aa
Dan Moore <dan@makeitdoathing.com>
2016-09-24 00:51:35 -0400

    updates

--- devApp/src/ofApp.cpp
+++ devApp/src/ofApp.cpp
@@ -24,7 +24,7 @@ void ofApp::setup(){
 void ofApp::update(){
     if(bRun){
 //        tcpNode.orbitDeg(sin(ofGetElapsedTimef()*0.01)*180, sin(ofGetElapsedTimef()*0.01)*180, 0.2, target);
-        tcpNode.setPosition(glm::lerp(glm::vec3(0.5, 0, 0), glm::vec3(0, 0.0, 0.5), glm::vec3(sin(ofGetElapsedTimef()*0.0412))));
+        tcpNode.setPosition(glm::lerp(glm::vec3(0.5, 0, 0), glm::vec3(0, 0.0, 0.5), glm::vec3(abs(sin(ofGetElapsedTimef()*0.0412)))));
 //        tcpNode.lookAt(glm::vec3(100, 0, 0), glm::vec3(0, 0, 1));
         model.update(tcpNode);
         
